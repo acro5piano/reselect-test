0e81e1bc0ad50e0f3b57069651820ff212b4a761
gosho-kazuya <ketsume0211@gmail.com>
2019-02-13 02:50:20 -0800

    refacotr

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -7,9 +7,13 @@ import styled from 'styled-components'
 const Container = styled.div`
   border: solid 1px #ccc;
   padding: 24px;
-  width: 524px;
-  height: 524px;
-  margin: 24px auto 0;
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  margin: 24px 0 0;
+`
+
+const Buttons = styled.div`
+  margin-bottom: 24px;
 `
 
 export class BarChart extends React.Component {
@@ -23,33 +27,37 @@ export class BarChart extends React.Component {
   }
 
   draw = () => {
-    const { data, size } = this.state
-    const node = this.node
-    const dataMax = max(data)
-    const yScale = scaleLinear()
-      .domain([0, dataMax])
-      .range([0, size])
+    const _draw = () => {
+      const { data, size } = this.state
+      const node = this.node
+      const dataMax = max(data)
+      const yScale = scaleLinear()
+        .domain([0, dataMax])
+        .range([0, size])
 
-    select(node)
-      .selectAll('rect')
-      .data(data)
-      .enter()
-      .append('rect')
+      select(node)
+        .selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
 
-    select(node)
-      .selectAll('rect')
-      .data(data)
-      .exit()
-      .remove()
+      select(node)
+        .selectAll('rect')
+        .data(data)
+        .exit()
+        .remove()
 
-    select(node)
-      .selectAll('rect')
-      .data(data)
-      .style('fill', '#fe9922')
-      .attr('x', (d, i) => i * 25)
-      .attr('y', d => size - yScale(d))
-      .attr('height', d => yScale(d))
-      .attr('width', 25)
+      select(node)
+        .selectAll('rect')
+        .data(data)
+        .style('fill', '#fe9922')
+        .attr('x', (d, i) => i * 25)
+        .attr('y', d => size - yScale(d))
+        .attr('height', d => yScale(d))
+        .attr('width', 25)
+    }
+
+    setImmediate(_draw)
   }
 
   bigger = () => {
@@ -64,24 +72,26 @@ export class BarChart extends React.Component {
 
   add = () => {
     this.setState({ data: [...this.state.data, Math.random() * 100] })
-    setImmediate(this.draw)
+    this.draw()
   }
 
   remove = () => {
     this.setState({ data: this.state.data.slice(0, -1) })
-    setImmediate(this.draw)
+    this.draw()
   }
 
   render() {
     const { size } = this.state
 
     return (
-      <Container>
+      <Container size={size + 24}>
+        <Buttons>
+          <button onClick={this.bigger}>bigger</button>
+          <button onClick={this.smaller}>smaller</button>
+          <button onClick={this.add}>add</button>
+          <button onClick={this.remove}>remove</button>
+        </Buttons>
         <svg ref={ref => (this.node = ref)} width={size} height={size} />
-        <button onClick={this.bigger}>bigger</button>
-        <button onClick={this.smaller}>smaller</button>
-        <button onClick={this.add}>add</button>
-        <button onClick={this.remove}>remove</button>
       </Container>
     )
   }
